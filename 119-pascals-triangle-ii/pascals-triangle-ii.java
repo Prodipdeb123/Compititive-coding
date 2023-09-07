f779f591bb3e93b9506f715d7a395cb04464ca23
PRODIP DEB <112300885+Prodipdeb123@users.noreply.github.com>
2023-09-07 23:35:17 +0530

    Time: 1 ms (78.04%) | Memory: 40.2 MB (53.58%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,13 +1,21 @@
-public class Solution {
-    public List<Integer> getRow(int k) {
-        Integer[] arr = new Integer[k + 1];
-        Arrays.fill(arr, 0);
-        arr[0] = 1;
+class Solution {
+    public List<Integer> getRow(int rowIndex) {
+        List<List<Integer>> ans = new ArrayList<>();
+        for(int i=0; i<=rowIndex; i++){
+            List<Integer> sum = new ArrayList<>();
+            
+            for(int j=0; j<=i; j++){
+                if(j==0 || j==i){
+                    sum.add(1);
+                }
+                else{
+                    sum.add(ans.get(i-1).get(j-1)+ans.get(i-1).get(j));
+                }
+            }
+            ans.add(sum);
+        }
+
+        return ans.get(rowIndex);
         
-        for (int i = 1; i <= k; i++) 
-            for (int j = i; j > 0; j--) 
-                arr[j] = arr[j] + arr[j - 1];
-        
-        return Arrays.asList(arr);
     }
 }
