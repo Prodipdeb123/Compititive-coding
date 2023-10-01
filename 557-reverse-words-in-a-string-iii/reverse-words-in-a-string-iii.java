4e822e9126761d80ae6c11d60e264f55fe9e0b4a
PRODIP DEB <112300885+Prodipdeb123@users.noreply.github.com>
2023-10-01 21:38:54 +0530

    Time: 4 ms (87.56%) | Memory: 44.1 MB (64.33%) - LeetSync

--- 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
+++ 557-reverse-words-in-a-string-iii/reverse-words-in-a-string-iii.java
@@ -2,8 +2,8 @@ class Solution {
     public String reverseWords(String s) {
       StringBuilder sb = new StringBuilder();
       String[] words= s.split(" ");
-      for(String word:words){
-          StringBuilder tmp= new StringBuilder(word);
+      for(int i = 0;i<words.length;i++){
+          StringBuilder tmp= new StringBuilder(words[i]);
           tmp.reverse();
           sb.append(tmp);
           sb.append(" ");
