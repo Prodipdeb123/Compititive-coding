64e70ed338923665f09eec1101464f3f815ad2b0
PRODIP DEB <112300885+Prodipdeb123@users.noreply.github.com>
2024-07-16 00:38:50 +0530

    Time: 0 ms (100.00%) | Memory: 45.7 MB (57.52%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,20 +1,54 @@
 class Solution {
+    //First occurance of element  
+    int firstOccurance(int[] nums, int n ,int x){
+        int low = 0 ,high = n-1;
+        int first = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == x){
+                first = mid;
+                high = mid - 1;
+            }else if(nums[mid] < x){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return first;
+    }
+
+    //last occurance of element
+    int lastOccurance(int[] nums,int n ,int x){
+        int low = 0 ,high = n-1;
+        int last = -1;
+
+        while(low <= high){
+            int mid = (low + high)/2;
+
+            if(nums[mid] == x){
+                last = mid;
+                low = mid + 1;
+            }else if(nums[mid] < x){
+                low = mid + 1;
+            }else{
+                high = mid - 1;
+            }
+        }
+
+        return last;
+
+    }
     public int[] searchRange(int[] nums, int target) {
-        int result[]=new int[2];
-        result[0]=-1;
-        result[1]=-1;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==target){
-                result[0]=i;
-                break;
-            }
-        }
-        for(int i=nums.length-1;i>=0;i--){
-            if(nums[i]==target){
-                result[1]=i;
-                break;
-            }
-        }
-        return result;
+        int n = nums.length; 
+        int first = firstOccurance(nums,n,target);
+
+        if(first == -1) return new int[] {-1,-1};
+        
+        int last = lastOccurance(nums,n,target);
+
+        return new int[] {first , last};
     }
 }
